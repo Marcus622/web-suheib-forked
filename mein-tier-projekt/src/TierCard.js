f0336255427b897cad6004ab9c9bd8db9e2e2920
Marcus622 <marcus.vix@tn.techstarter.de>
2025-04-16 17:41:06 +0200

    Tiercard

---
+++ mein-tier-projekt/src/TierCard.js
@@ -0,0 +1,21 @@
+// TierCard.js
+function TierCard({ name, art, alter, bild }) {
+    return (
+      <div style={{
+        border: "2px solid #ccc",
+        borderRadius: "10px",
+        padding: "10px",
+        margin: "10px",
+        width: "250px",
+        boxShadow: "2px 2px 10px rgba(0,0,0,0.1)"
+      }}>
+        <img src={bild} alt={name} style={{ width: "100%", borderRadius: "10px" }} />
+        <h3>{name}</h3>
+        <p><strong>Art:</strong> {art}</p>
+        <p><strong>Alter:</strong> {alter} Jahre</p>
+      </div>
+    );
+  }
+  
+  export default TierCard;
+  
---
+++ mein-tier-projekt/src/Titel.js
@@ -0,0 +1,7 @@
+
+function Titel() {
+    return (
+    <h1 style={{backgroundColor: "red", textAlign: "center", fontSize: "30px", border: "solid"}}>Die kleine Tiercard</h1>
+    );
+}
+export default Titel;
